5dc226e30bcc4b331043aeeb10ad7a7096f578f0
IsiDevelopment <40302671+IsiDevelopment@users.noreply.github.com>
2018-10-05 23:44:57 -0500

    Add files via upload

--- JavaScript/parallax_effect.js
+++ JavaScript/parallax_effect.js
@@ -1,9 +1,7 @@
   var ypos, image;
-
   function parallex() {
     ypos = window.pageYOffset
-    image = document.getElementById('image')
+    image = document.getElementById('image_font')
     image.style.top = ypos * .5 + 'px'
   }
-  
   window.addEventListener('scroll', parallex);
