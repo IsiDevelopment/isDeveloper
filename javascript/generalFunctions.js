d6f4ae12eff43d94094e071e10b7e76473e4477d
isidro <isidro.cano.mtz@outlook.es>
2020-01-01 22:02:06 -0600

    new year

--- javascript/generalFunctions.js
+++ javascript/generalFunctions.js
@@ -3,9 +3,25 @@ function hideDiv(item){
     $('#' + item).slideToggle('slow')
   });
 }
+//local = http://192.168.0.6/isdeveloper/
+//github = https://isidevelopment.github.io/isdeveloper/
+//production = http://www.isidevelopment.com/
+var environment = 'https://isidevelopment.github.io/isdeveloper/'
+function gotoHome(){
+  location.href = environment
+}
 function gotoPDV(){
-  location.href = 'http://192.168.0.6/isdeveloper/pdv'
+  location.href = environment + 'pdv'
+}
+function gotoWebSites(){
+  location.href = environment + 'websites'
 }
 function gotoCustomized(){
-  location.href = 'http://192.168.0.6/isdeveloper/customized'
+  location.href = environment + 'customized'
+}
+function gotoAboutUs(){
+  location.href = environment + 'aboutus'
+}
+function gotoContact(){
+  location.href = environment + '#contact'
 }
